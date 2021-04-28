da3c075cb706abd6d410e6a52acb05dc30ff89a5
Romeo <baobab_man@kakao.com>
2021-04-28 21:25:33 +0900

    baekjoon 2-4
    baekjoon 2-4

--- baekjoon/2단계_if문/step_2.py
+++ baekjoon/2단계_if문/step_2.py
@@ -34,3 +34,19 @@ if (a % 4 == 0) and ((a % 100 != 0) or (a % 400 == 0)):
     print(1)
 else:
     print(0)
+
+
+# 2-4
+# 첫 줄에는 정수 x가 주어진다. (−1000 ≤ x ≤ 1000; x ≠ 0) 다음 줄에는 정수 y가 주어진다. (−1000 ≤ y ≤ 1000; y ≠ 0)
+# 점 (x, y)의 사분면 번호(1, 2, 3, 4 중 하나)를 출력한다.
+
+x = int(input())
+y = int(input())
+if x > 0 and y > 0:
+    print(1)
+elif x < 0 and y > 0:
+    print(2)
+elif x < 0 and y < 0:
+    print(3)
+else:
+    print(4)
