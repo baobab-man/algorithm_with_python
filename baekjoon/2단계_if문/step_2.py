d8c691c9225007623f1053fa0d4bee59d64b000d
Romeo <baobab_man@kakao.com>
2021-04-28 21:25:08 +0900

    baekjoon 2-3
    baekjoon 2-3

--- baekjoon/2단계_if문/step_2.py
+++ baekjoon/2단계_if문/step_2.py
@@ -23,3 +23,14 @@ else:
 
 a = int(input())
 print('A' if a >= 90 else ('B' if a >= 80 else ('C' if a >= 70 else('D' if a >=60 else('F')))))
+
+
+# 2-3
+# 첫째 줄에 연도가 주어진다. 연도는 1보다 크거나 같고, 4000보다 작거나 같은 자연수이다.
+# 첫째 줄에 윤년이면 1, 아니면 0을 출력한다.
+
+a = int(input())
+if (a % 4 == 0) and ((a % 100 != 0) or (a % 400 == 0)):
+    print(1)
+else:
+    print(0)
