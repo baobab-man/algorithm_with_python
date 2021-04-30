915c5de76639eb732a52c7f67307284a2fa55043
Romeo <baobab_man@kakao.com>
2021-04-30 13:36:50 +0900

    baekjoon 3-3
    baekjoon 3-3

--- baekjoon/3단계_for문/step_3.py
+++ baekjoon/3단계_for문/step_3.py
@@ -16,3 +16,14 @@ t = int(input())
 for i in range(t):
     a, b = map(int, input().split())
     print(a + b)
+
+
+# 3-3
+# 첫째 줄에 n (1 ≤ n ≤ 10,000)이 주어진다.
+# 1부터 n까지 합을 출력한다.
+
+n = int(input())
+sum = 0
+for i in range(1, n+1):
+    sum = sum + i
+print(sum)
