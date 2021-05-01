ab1435bea5a6387b2f7fc9d7cc0ad60afa33db18
Romeo <baobab_man@kakao.com>
2021-05-01 18:46:52 +0900

    baekjoon 3-5
    baekjoon 3-5

--- baekjoon/3단계_for문/step_3.py
+++ baekjoon/3단계_for문/step_3.py
@@ -38,3 +38,12 @@ t = int(sys.stdin.readline())
 for i in range(t):
     a, b = map(int, sys.stdin.readline().split())
     print(a + b)
+
+
+# 3-5
+# 첫째 줄에 100,000보다 작거나 같은 자연수 N이 주어진다.
+# 첫째 줄부터 N번째 줄 까지 차례대로 출력한다.
+
+a = int(input())
+for i in range(a):
+    print(i+1)
