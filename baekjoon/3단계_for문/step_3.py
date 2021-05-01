0fab74c81d19dfca5aac7309248b0ab05c1f3c4d
Romeo <baobab_man@kakao.com>
2021-05-01 18:46:33 +0900

    baekjoon 3-4
    baekjoon 3-4

--- baekjoon/3단계_for문/step_3.py
+++ baekjoon/3단계_for문/step_3.py
@@ -27,3 +27,14 @@ sum = 0
 for i in range(1, n+1):
     sum = sum + i
 print(sum)
+
+
+# 3-4
+# 첫 줄에 테스트케이스의 개수 T가 주어진다. T는 최대 1,000,000이다. 다음 T줄에는 각각 두 정수 A와 B가 주어진다. A와 B는 1 이상, 1,000 이하이다.
+# 각 테스트케이스마다 A+B를 한 줄에 하나씩 순서대로 출력한다.
+import sys
+
+t = int(sys.stdin.readline())
+for i in range(t):
+    a, b = map(int, sys.stdin.readline().split())
+    print(a + b)
