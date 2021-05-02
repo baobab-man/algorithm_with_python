462122d5b20db2429c72cc65ce55129807a9e87f
Romeo <baobab_man@kakao.com>
2021-05-02 23:58:13 +0900

    baekjoon 4-2
    baekjoon 4-2

--- baekjoon/4단계_while문/step_4.py
+++ baekjoon/4단계_while문/step_4.py
@@ -10,3 +10,16 @@ while True:
         print(a + b)
     else:
         break
+
+
+# 4-2
+# 입력은 여러 개의 테스트 케이스로 이루어져 있다.
+# 각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
+# 각 테스트 케이스마다 A+B를 출력한다.
+
+while 1:
+    try:
+        a, b = map(int, input().split())
+        print(a + b)
+    except:
+        break
