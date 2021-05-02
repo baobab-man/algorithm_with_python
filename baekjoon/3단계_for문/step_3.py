ff3d59df378e33384ed387f3216468e911104c96
Romeo <baobab_man@kakao.com>
2021-05-02 23:28:29 +0900

    baekjoon 3-11
    baekjoon 3-11

--- baekjoon/3단계_for문/step_3.py
+++ baekjoon/3단계_for문/step_3.py
@@ -96,3 +96,15 @@ for i in range(n):
 n = int(input())
 for i in range(1, n+1):
     print("{}".format(i*"*").rjust(n))
+
+
+# 3-11
+# 첫째 줄에 N과 X가 주어진다. (1 ≤ N, X ≤ 10,000)
+# 둘째 줄에 수열 A를 이루는 정수 N개가 주어진다. 주어지는 정수는 모두 1보다 크거나 같고, 10,000보다 작거나 같은 정수이다.
+# X보다 작은 수를 입력받은 순서대로 공백으로 구분해 출력한다. X보다 작은 수는 적어도 하나 존재한다.
+
+n, x = map(int, input().split())
+array = list(map(int, input().split()))
+for i in range(n):
+    if array[i] < x:
+        print(array[i], end=" ")
