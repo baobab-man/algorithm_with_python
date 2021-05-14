d3bb5b241bdd148ba4204163a7b82b5a01890318
Romeo <baobab_man@kakao.com>
2021-05-14 09:33:59 +0900

    baekjoon 5-6
    baekjoon 5-6

--- baekjoon/5단계_1차원배열/step_5.py
+++ baekjoon/5단계_1차원배열/step_5.py
@@ -74,3 +74,19 @@ for i in range(test_no):
             score = 0
     print(sum_score)
 
+
+# 5-7
+# 대학생 새내기들의 90%는 자신이 반에서 평균은 넘는다고 생각한다. 당신은 그들에게 슬픈 진실을 알려줘야 한다.
+# 첫째 줄에는 테스트 케이스의 개수 C가 주어진다.
+# 둘째 줄부터 각 테스트 케이스마다 학생의 수 N(1 ≤ N ≤ 1000, N은 정수)이 첫 수로 주어지고, 이어서 N명의 점수가 주어진다. 점수는 0보다 크거나 같고, 100보다 작거나 같은 정수이다.
+# 각 케이스마다 한 줄씩 평균을 넘는 학생들의 비율을 반올림하여 소수점 셋째 자리까지 출력한다.
+
+test_no = int(input())
+for _ in range(test_no):
+    score = list(map(int, input().split()))
+    average = sum(score[1:])/score[0]
+    count = 0
+    for i in score[1:]:
+        if i > average:
+            count += 1
+    print("%.3f%%" % round(count/score[0]*100, 3))
